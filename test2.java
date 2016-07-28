3e9c865d8ae75a4d17883914c92eb4ff605026a9
nuomituanziM <hedyhma@gmail.com>
2016-07-27 17:57:41 -0700

    Modify test in 07/27 in testDayTwo
    Modify test2 in 07/27 in testDayTwo
    
    Modify test3 on 07/27 in testDayTwo

--- test2.java
+++ test2.java
@@ -1,2 +1,3 @@
 /* This is the 2nd commit in 07/25 */
 /* Can I add this into 07/25's commit? */
+/* 05/57 07/27 */
--- test3.java
+++ test3.java
@@ -1,1 +1,2 @@
 /* This is a test3 file made on 16:50 07/27 */
+/* 05:59 07/27 */
